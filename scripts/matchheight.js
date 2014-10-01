5cca112c701c97192f8f976c0b8b5ceb0f606b76
John Eberle <john@clusterag.leiningergymnasium.de>
2014-10-01 15:44:00 +0200

    added matchheight.js

--- scripts/matchheight.js
+++ scripts/matchheight.js
@@ -288,3 +288,24 @@
     });
 
 })(jQuery);
+
+
+
+(function() {
+
+                /* matchHeight */
+
+                $(function() {
+                    // apply your matchHeight on DOM ready (they will be automatically re-applied on load or resize)
+                    var byRow = "true";
+
+                    // apply matchHeight to each item container's items
+                    $('.matchheight').each(function() {
+                        $(this).children('.item').matchHeight(byRow);
+                    });
+
+                   ;
+
+                });
+
+            })();
